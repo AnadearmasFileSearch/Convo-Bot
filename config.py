74cc777a2843d11a174491f1126fbad28f3d0d25
AnadearmasFileSearch <anad53951@gmail.com>
2024-12-15 20:26:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 import os
 
-BOT_TOKEN = os.getenv('BOT_TOKEN')  # Read the bot token from environment variables
-MONGO_URL = os.getenv('MONGO_URL')  # MongoDB connection URL from environment variables
-ADMIN_ID = os.getenv('ADMIN_ID')  # Admin ID from environment variables
-WEBHOOK_URL = os.getenv('WEBHOOK_URL')  # Webhook URL from environment variables
+# Bot credentials and settings
+BOT_TOKEN = "7524278392:AAGd21uCuExw-JAuko9UC5l9GLrqmwy-mh4"
+ADMIN_ID = 1490611763
+WEBHOOK_URL = "https://convo-bot-4hl6.onrender.com"
+MONGO_URI = "mongodb+srv://NewFileShareBoT:EenaMeena@cluster0.7rbuejp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
