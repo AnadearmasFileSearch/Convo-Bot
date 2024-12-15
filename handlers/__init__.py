3c406f49ac1422697da46ca15e4aadb1aa0dba20
AnadearmasFileSearch <anad53951@gmail.com>
2024-12-15 19:47:50 +0530

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,5 +1,6 @@
+# handlers/__init__.py
 from .start import start
-from .forward import forward_message_to_admin
+from .forward import forward_message_to_admin  # Ensure this line exists
 from .reply import reply_to_user
 from .broadcast import broadcast
 from .users import users
