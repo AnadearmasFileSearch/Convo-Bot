ef8e04a3d9d8c6cb4e4a33130d35f0310f5c9de5
AnadearmasFileSearch <anad53951@gmail.com>
2024-12-15 20:00:17 +0530

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,7 +1,5 @@
-# handlers/__init__.py
-
 from .start import start
 from .forward import forward_message_to_admin
 from .reply import reply_to_user
 from .broadcast import broadcast
-from .users import save_user  # Make sure this import matches your users.py file
+from .users import save_user
