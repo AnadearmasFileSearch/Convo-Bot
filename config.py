8df8bbb4080a1a8bc16cb2dc47ef5f3735a9b0b4
AnadearmasFileSearch <anad53951@gmail.com>
2024-12-15 20:30:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,6 @@
-import os
+# config.py
 
-# Bot credentials and settings
 BOT_TOKEN = "7524278392:AAGd21uCuExw-JAuko9UC5l9GLrqmwy-mh4"
 ADMIN_ID = 1490611763
 WEBHOOK_URL = "https://convo-bot-4hl6.onrender.com"
-MONGO_URI = "mongodb+srv://NewFileShareBoT:EenaMeena@cluster0.7rbuejp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+MONGO_URL = "mongodb+srv://NewFileShareBoT:EenaMeena@cluster0.7rbuejp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
